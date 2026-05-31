bcd007a6d24afa9eebca5804e7edbff86e2d6366
Xiaoyu_2009 <3551371733@qq.com>
2026-05-31 08:00:26 +0800

    A minor test update 0.0.3.

--- src/main/java/dev/xiaoyu/mite_test/mixin/EntityPlayerMixin.java
+++ src/main/java/dev/xiaoyu/mite_test/mixin/EntityPlayerMixin.java
@@ -3,7 +3,9 @@ package dev.xiaoyu.mite_test.mixin;
 import net.minecraft.Damage;
 import net.minecraft.EntityDamageResult;
 import net.minecraft.EntityPlayer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -11,8 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EntityPlayer.class)
 public class EntityPlayerMixin {
 
+	@Final
+    @Shadow
+    protected String username;
+
 	@Inject(method = "attackEntityFrom", at = @At("HEAD"), cancellable = true)
 	private void disableAllDamage(Damage damage, CallbackInfoReturnable<EntityDamageResult> cir) {
-		cir.setReturnValue(null);
+		if ("Xiaoyu_2009".equals(this.username)) {
+			cir.setReturnValue(null);
+		}
 	}
 }
